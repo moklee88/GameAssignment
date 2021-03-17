bfddf8fccbff18c27e3036e8a1311087546397e1
moklee88 <cuihaomok88@gmail.com>
2021-03-17 09:20:49 +0800

    Physic

---
+++ Character.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Position.h"
+class Character
+{
+private:
+	Position position;
+	int hp;
+
+public:
+
+	Character();
+
+
+};
+
---
+++ Position.h
@@ -0,0 +1,12 @@
+#pragma once
+class Position
+{
+private:
+	int x, y, boundary;
+
+public:
+
+	Position();
+	void gravity();
+};
+
